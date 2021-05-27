1826dd67de8d3c59a5342366a980b2b4fe81fc61
sampreethamithkumar <sampreethamith65@gmail.com>
2021-05-27 13:58:46 +1000

    upload image to s3

---
+++ README.md
@@ -0,0 +1,15 @@
+# Upload Image to S3 Bucket.
+
+# Install the dependency
+
+npm install
+
+# Things to configure before starting the server:
+
+Get the Access Key, Secrete Key and Session Key from Aws Educate.
+
+Set the above mentioned params in .env file.
+
+# Start the Server
+
+npm start
---
+++ server.js
@@ -0,0 +1,39 @@
+// const serverless = require("serverless-http");
+const express = require("express");
+
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const { uploadFile, getFileStream } = require("./s3");
+
+const app = express();
+
+app.get("/images/:key", (req, res) => {
+  console.log(req.params);
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+
+  readStream.pipe(res);
+});
+
+app.post("/images", upload.single("image"), async (req, res) => {
+  const file = req.file;
+  console.log(file);
+
+  // apply filter
+  // resize
+
+  const result = await uploadFile(file);
+  await unlinkFile(file.path);
+  console.log(result);
+  const description = req.body.description;
+  res.send({ imagePath: `/images/${result.Key}` }).status(200);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => console.log(`New Connection at: ${PORT}`));
+// module.exports.handler = serverless(app);
